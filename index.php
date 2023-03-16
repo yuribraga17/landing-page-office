03a71e7152b0efc82bf8a3b0d815f332c0315003
Yuri Braga <yuribragasoares@gmail.com>
2023-03-15 21:43:53 -0300

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,6 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Yuri Dev</title>
-	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="css/style.css">
   <script src="https://kit.fontawesome.com/002f4cf7f8.js" crossorigin="anonymous"></script>
